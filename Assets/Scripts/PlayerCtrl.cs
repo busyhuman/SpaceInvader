2c3a79cec1b71354b223c3b3686cc1260ab8c3ea
busyhuman <busyhuman@naver.com>
2020-05-05 21:41:58 +0900

    Ver1.0

---
+++ Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditorInternal;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public float speed = 20.0f;
+
+    Vector2 touchPosition;
+    Camera Camera;
+    bool isMove = false;
+
+    void Awake()
+    {
+        Camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch t = Input.GetTouch(0);
+
+            if(t.phase == TouchPhase.Began)
+            {
+                touchPosition = t.position;
+                touchPosition = Camera.ScreenToWorldPoint(touchPosition);
+                isMove = true;
+            }
+        }
+
+        if (isMove)
+        {
+            if (Vector2.Distance(transform.position, touchPosition) < 0.01f)
+            {
+                isMove = false;
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, touchPosition, speed * Time.deltaTime);
+            }
+                
+        }
+        
+    }
+
+}
